6b036888187c80ff78664d5af4d6f741d2140ea2
Gaurav Mittal <61792468+gauravmittal54@users.noreply.github.com>
2024-02-12 11:55:01 +0530

    Create swagger.js

---
+++ swagger.js
@@ -0,0 +1,18 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const outputFile = './swagger_output.json';
+const endpointsFiles = ['./api/users/user.router.js'];
+
+const doc = {
+  info: {
+    title: 'User API',
+    description: 'API documentation for managing users',
+  },
+  host: 'localhost:3000',
+  basePath: '/api/users',
+  schemes: ['http'],
+  consumes: ['application/json'],
+  produces: ['application/json'],
+};
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
