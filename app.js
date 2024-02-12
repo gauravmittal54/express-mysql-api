0151d55b256484d96eb6ef8c55089c41cefd2567
Gaurav Mittal <61792468+gauravmittal54@users.noreply.github.com>
2024-02-12 12:05:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger_output.json');
 const userRouter = require('./api/users/user.router');
 
+const port = process.env.APP_PORT || 3000;
+global.serverUrl = process.env.SERVER_URL || `http://localhost:${port}`;
+
 app.use(express.json());
 
 app.use('/api/users' , userRouter);
