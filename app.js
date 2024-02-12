3b66420ba2b911cd06f4fe9f517507af1d0f3d10
Gaurav Mittal <61792468+gauravmittal54@users.noreply.github.com>
2024-02-12 11:56:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger_output.json');
 const userRouter = require('./api/users/user.router');
 
 app.use(express.json());
 
 app.use('/api/users' , userRouter);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(process.env.APP_PORT,() =>{
     console.log('server running at port 3000')
